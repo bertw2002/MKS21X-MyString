dd7475f9c8a114e92a440f95ada1960b812ac442
Albert Wan <awan00@stuy.edu>
2018-11-21 11:00:31 -0500

    finished all methods but subsequence

--- MyString.java
+++ MyString.java
@@ -1,1 +1,33 @@
-
+public class MyString implements CharSequence,Comparable<CharSequence>{
+ 	private char[] data;
+ 	public MyString(CharSequence s){
+		data = s;
+	}
+	//Returns a string containing the characters in this sequence in the same order as this sequence.
+	public String toString(){
+		String var = "";
+		for (int x = 0; x < data.length; x++){
+			var += data[x];
+		}
+		return var; 
+	}
+	//Returns the char value at the specified index.
+	public char charAt(int index){
+		if (index < 0 || index >= data.length){
+			throw new IndexOutofBoundsException();
+		}
+		return data(index);
+	}
+	//Returns the length of this character sequence.
+	public int length(){
+		return data.length;
+	}
+	//Returns a CharSequence that is a subsequence of this sequence.
+	public CharSequence subsequence(int start, int end){
+		if (start < 0 || end < 0 || end > data.length){
+			throw new IndexOutofBoundsException();
+		}
+		int length = end - start;
+		CharSequence newarr = new CharSequence[length];
+	}
+}
