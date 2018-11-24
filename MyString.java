e6f9208731837f5af25ecdaab72961cefbbd7c2c
Sublime <other@Alberts-MBP.fios-router.home>
2018-11-24 00:38:39 -0500

    started compareto

--- MyString.java
+++ MyString.java
@@ -36,6 +36,44 @@ public class MyString implements CharSequence, Comparable<CharSequence>{
       val += data[x];
     }
     return val;
-
 	}
+  //Compares this object with the specified object for order. (Mystring with this charsequence)
+  public int compareTo(CharSequence s){
+    if (s == null){
+      throw new NullPointerException("null value");
+    }
+    //check size first because data[x] or s.charat(x) may be index out of bounds
+    int preventerror = 0;
+
+    if (length() > s.length()) {
+      preventerror = 1;
+    }
+    if (length() < s.length()){
+      preventerror = -1;
+    }
+    //bigger or smaller
+    if (preventerror == -1){
+      //if s is smaller, make it not go further cuz it will throw error
+      for (int x = 0; x < s.length(); x++){
+        if (data[x] > s.charAt(x)) {
+          return 1;
+        }
+        if (data[x] > s.charAt(x)){
+          return -1;
+        }
+      }
+    }
+    //vice versa
+    if(preventerror == 1){
+      for (int x = 0; x < data.length; x++){
+        if (data[x] > s.charAt(x)) {
+          return 1;
+        }
+        if (data[x] > s.charAt(x)){
+          return -1;
+        }
+      }
+    }
+    return preventerror;
+  }
 }
